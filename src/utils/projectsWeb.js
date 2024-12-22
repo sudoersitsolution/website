f723dffff7dc888f8849218f7d110336b1d7b61a
ABBAS <abbasas690@gmail.com>
2024-12-22 22:25:46 +0530

    added web development in project section

--- src/utils/projectsWeb.js
+++ src/utils/projectsWeb.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-const projectAddItem = () => {
-  const projects = [
+const projectAddItemWeb = () => {
+  const projectsWeb= [
     { title: "Chronic Obstructive Pulmonary Disease Prediction System", link: "" },
     { title: "Liver Cirrhosis Prediction System using Random Forest", link: "" },
     { title: "Multiple Disease Prediction System using Machine Learning", link: "" },
@@ -31,62 +30,61 @@ const projectAddItem = () => {
     { title: "Sleep Time Prediction", link: "" },
   ];
 
-  const itemsPerPage = 5; // Number of items per page
-  let currentPage = 1; // Current page index
-  const totalPages = Math.ceil(projects.length / itemsPerPage);
+  const itemsPerPageWeb = 5; // Number of items per page
+  let currentPageWeb = 1; // Current page index
+  const totalPagesWeb = Math.ceil(projectsWeb.length / itemsPerPageWeb);
 
-  const renderProjects = () => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const visibleProjects = projects.slice(startIndex, endIndex);
+  const renderProjectsWeb = () => {
+    const startIndexWeb = (currentPageWeb - 1) * itemsPerPageWeb;
+    const endIndexWeb = startIndexWeb + itemsPerPageWeb;
+    const visibleProjectsWeb = projectsWeb.slice(startIndexWeb, endIndexWeb);
 
-    let elements = "";
-    visibleProjects.forEach((item, index) => {
-      elements += `
+    let elementsWeb = "";
+    visibleProjectsWeb.forEach((item, index) => {
+      elementsWeb += `
         <div class="project__item">
-          <p>${startIndex + index + 1}. ${item.title}</p>
+          <p>${startIndexWeb + index + 1}. ${item.title}</p>
           <a href="${item.link}">document</a>  
         </div>
       `;
     });
-    const PreviousButton=`
+    const PreviousButtonWeb=`
 <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.017 1.995c5.517 0 9.997 4.48 9.997 9.998s-4.48 9.998-9.997 9.998c-5.518 0-9.998-4.48-9.998-9.998s4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.498 8.498 8.498 8.497-3.808 8.497-8.498-3.807-8.498-8.497-8.498zm-1.528 4.715s-1.502 1.505-3.255 3.259c-.147.147-.22.339-.22.531s.073.383.22.53c1.753 1.754 3.254 3.258 3.254 3.258.145.145.335.217.526.217.192-.001.384-.074.531-.221.292-.293.294-.766.003-1.057l-1.977-1.977h6.693c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-6.693l1.978-1.979c.29-.289.287-.762-.006-1.054-.147-.147-.339-.221-.53-.222-.19 0-.38.071-.524.215z" fill-rule="nonzero"/></svg>
     `
-    const nextButton =`
+    const nextButtonWeb =`
     <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.012 1.995c-5.518 0-9.998 4.48-9.998 9.998s4.48 9.998 9.998 9.998 9.997-4.48 9.997-9.998-4.479-9.998-9.997-9.998zm0 1.5c4.69 0 8.497 3.808 8.497 8.498s-3.807 8.498-8.497 8.498-8.498-3.808-8.498-8.498 3.808-8.498 8.498-8.498zm1.528 4.715s1.502 1.505 3.255 3.259c.146.147.219.339.219.531s-.073.383-.219.53c-1.753 1.754-3.254 3.258-3.254 3.258-.145.145-.336.217-.527.217-.191-.001-.383-.074-.53-.221-.293-.293-.295-.766-.004-1.057l1.978-1.977h-6.694c-.414 0-.75-.336-.75-.75s.336-.75.75-.75h6.694l-1.979-1.979c-.289-.289-.286-.762.006-1.054.147-.147.339-.221.531-.222.19 0 .38.071.524.215z" fill-rule="nonzero"/></svg>
 
     `
 
-    const projectList = document.querySelector(".project__list");
-    projectList.innerHTML = elements;
+    const projectListWeb = document.querySelector(".project__list-web");
+    projectListWeb.innerHTML = elementsWeb;
 
-    const paginationControls = `
-      <button ${currentPage === 1 ? "disabled" : ""} id="prev">${PreviousButton}</button>
-      <span>Page ${currentPage} of ${totalPages}</span>
-      <button ${currentPage === totalPages ? "disabled" : ""} id="next">${nextButton}</button>
+    const paginationControlsWeb = `
+      <button ${currentPageWeb === 1 ? "disabled" : ""} id="prev-web">${PreviousButtonWeb}</button>
+      <span>Page ${currentPageWeb} of ${totalPagesWeb}</span>
+      <button ${currentPageWeb === totalPagesWeb ? "disabled" : ""} id="next-web" >${nextButtonWeb}</button>
     `;
 
-    const paginationContainer = document.querySelector(".pagination");
-    paginationContainer.innerHTML = paginationControls;
+    const paginationContainerWeb = document.querySelector(".pagination-web");
+    paginationContainerWeb.innerHTML = paginationControlsWeb;
 
     // Add event listeners for buttons
-    document.getElementById("prev").addEventListener("click", () => {
-      if (currentPage > 1) {
-        currentPage--;
-        renderProjects();
+    document.getElementById("prev-web").addEventListener("click", () => {
+      if (currentPageWeb > 1) {
+        currentPageWeb--;
+        renderProjectsWeb();
       }
     });
 
-    document.getElementById("next").addEventListener("click", () => {
-      if (currentPage < totalPages) {
-        currentPage++;
-        renderProjects();
+    document.getElementById("next-web").addEventListener("click", () => {
+      if (currentPageWeb < totalPagesWeb) {
+        currentPageWeb++;
+        renderProjectsWeb();
       }
     });
   };
 
-  renderProjects();
+  renderProjectsWeb();
 };
 
-projectAddItem();
-})
+projectAddItemWeb();
